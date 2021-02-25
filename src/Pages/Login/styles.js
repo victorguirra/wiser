1e1fb17a57d1d828096a6280a2ce2e38514d9004
victorguirra <victor.hugo.guirra@gmail.com>
2021-02-24 21:14:02 -0300

    updated media queries

--- src/Pages/Login/styles.js
+++ src/Pages/Login/styles.js
@@ -31,10 +31,21 @@ export const ImgContainer = styled.div`
         height:100%;
         object-fit:cover;
     }
+
+    @media(max-width:649px){
+        width:100%;
+        height:100vh;
+
+        &:after{
+            width:100%;
+        }
+    }
 `;
 
 export const FormContainer = styled.div`
     width:55%;
+
+    background-color:${ props => props.theme.colors.white };
 
     display:flex;
     align-items:center;
@@ -77,8 +88,85 @@ export const FormContainer = styled.div`
 
         margin-top:30px;
     }
+
+    @media(min-width:1348px){        
+        form{
+            width:50%;
+        }
+    }
+
+    @media(min-width:650px) and (max-width:1347px){
+        form{
+            width:80%;
+        }
+    }
+
+    @media(max-width:649px){
+        width:85%;
+        height:45%;
+        min-height:450px;
+
+        position:absolute;
+        
+        bottom:0;
+        top:0;
+        left:0;
+        right:0;    
+        margin:auto;
+
+        border-radius:10px;
+
+        h2{
+            font-size:24px;
+            line-height:32px;
+            text-align:center;
+        }
+
+        p{
+            font-size:14px;
+            text-align:center;
+            margin-bottom:25px;
+
+            br{
+                display:none;
+            }
+        }
+
+        form{
+            width:90%;
+            height:100%;
+            position:relative;
+            padding-top:25px;
+        }
+
+        button{
+            width:60%;
+            position:absolute;
+            bottom:-20px;
+            box-shadow:none;
+            left:0;
+            right:0;
+            margin:0 auto;
+        }
+
+        p:last-of-type{
+            color:${ props => props.theme.colors.white };
+
+            position:absolute !important;
+            bottom:-100px;
+            text-align:center;
+            left:0;
+            right:0;
+            margin:0 auto;
+        }
+
+    }
 `;
 
 export const ForgotPasswordLink = styled(Link)`
     color:${ props => props.theme.colors.purple };
+
+    @media(max-width:649px){
+        color:${ props => props.theme.colors.white };
+    }
 `;
